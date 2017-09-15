2fe58dc31e1709a6081e601a8fabf3b5cb4558b7
Christian Reimer <creimer@mudbox.org>
2017-09-14 21:48:11 -0400

    Added tests for data generator

---
+++ tests/test_data.py
@@ -0,0 +1,24 @@
+from app import data
+import types
+
+
+def test_data_generator():
+    gen = data.auction_generator()
+    assert isinstance(gen, types.GeneratorType)
+
+
+def test_data_create():
+    gen = data.auction_generator()
+    item = gen.__next__()
+    assert item['description'] in data._descriptions
+    assert not item['last_bid']['user']
+
+
+def test_data_update():
+    gen = data.auction_generator()
+    item = gen.__next__()
+    item = gen.__next__()
+    assert item['description'] in data._descriptions
+    assert item['last_bid']['user'] in data._user_names
+    assert item['last_bid']['price'] > 0
+
